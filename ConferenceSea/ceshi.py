8eea8a033225a14e48f94d79efe8e34993eeea65
chenzhiyou <chenzhiyou0320@163.com>
2018-03-26 11:38:33 +0800

    抽取会议信息

--- ConferenceSea/ceshi.py
+++ ConferenceSea/ceshi.py
@@ -1,49 +1,42 @@
 # coding=utf-8
 from selenium import webdriver
 import urllib
+import re
 from lxml import etree
+comtent = open('./tes.html')
+Month = {
+    'Jan': '01',
+    'Feb' : '02',
+    'Mar' : '03',
+    'Apr' : '04',
+    'May' : '05',
+    'Jun' : '06',
+    'Jul' : '07',
+    'Aug' : '08',
+    'Sep' : '09',
+    'Oct' :'10',
+    'Nov' : '11',
+    'Dec' : '12'
+}
 
-# driver = webdriver.PhantomJS()
-# driver.get('./tes.html')
-# datas = driver.find_elements_by_xpath('//a')
-# print type(datas)
-# for data in datas:
-#     print data
-#
-
-
-
-# end = {'custom_enddate': '01/02/2017'}
-# end = urllib.urlencode(end)
-# print end
-html = """
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-    <p>hello</p>
-    <p>world</p>
-    <a href="www.dfds">a1</a>
-    <a href="dfada">a2</a>
-    <div>div1</div>
-
-
-</body>
-</html>
-"""
+html = comtent.read()
 html = etree.HTML(html)
-a = html.xpath('//a[1]/text()')
-b = html.xpath('//div')
-print a
-print b
-c = """
-                                        JW Marriott Phoenix Desert Ridge Resort & Spa
-5350 E Marriott Dr
-Phoenix, Arizona, United States Of America
+a = html.xpath('//div[@class="date"]/text()')[0]
+# b = html.xpath('//div')
+a = a.replace(',', ' ').replace('|', ' ').strip()
+date_info = re.match(r'([^ ]+).*?([\d]{1,2}).*?([\d]{1,2}).*?([\d]{4})', a)
+month =  date_info.group(1)
+start_day =  date_info.group(2)
+end_day =  date_info.group(3)
+year =  date_info.group(4)
 
-"""
-c = c.strip()
-print c
+start_date = year + '-' + Month.get(month) + '-' + start_day
+print start_date
+area = html.xpath('//div[@class="date"]/a/text()')
+print area[0]
+print area[1]
+area = area[0] + ',' + area[1]
+print area
+
+
+
